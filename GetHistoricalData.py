0c675e43b2140b0a098e4f2158e784d0f225019c
sonnyDAC <105025000+sonnyDAC@users.noreply.github.com>
2022-06-23 12:24:33 +1000

    Update GetHistoricalData.py

--- GetHistoricalData.py
+++ GetHistoricalData.py
@@ -3,34 +3,31 @@ from datetime import datetime
 import pandas as pd
 import ccxt
 
-
 with open('config.json', 'r') as f: 
     config = json.load(f)
 
-api_key = config['exchange']['apiKey']
+api_key = config['exchange']['apiKey'] # enter keys in config.json 
 api_secret = config['exchange']['secret']
 
-exchange = ccxt.ftx()
+exchange = ccxt.ftx() # change to your exchange
 exchange_class = getattr(ccxt, 'ftx')
 exchange = exchange_class({
-    'apiKey': "",
-    'secret': "",
+    'apiKey': api_key,
+    'secret': api_secret,
     # 'asyncio_loop': self.loop,
-    'enableRateLimit': False,
+    'enableRateLimit': True,
     'test': False,
 })
 
+# use function in FindTickers.py to find CCXT unified tickers from the given exchange
+# Perpetuals
+ftx_symbols = ['BTC/USD:USD', 'ETH/USD:USD', 'SOL/USD:USD', 'CRO/USD:USD', 'GLMR/USD:USD', 'BNB/USD:USD', 'LTC/USD:USD']
+ftx_futures = ['BTC/USD:USD-220624']
 bitmex_symbols = ['BTC/USDT:USDT', 'ETH/USDT:USDT', 'SOL/USDT:USDT']
 bitmex_futures = ['BTC/USDT:USDT-220624']
 
 ## To get historical data for expired contracts, use exchange specific symbols
-params_bitmex = {'market_name': 'ETHJ17'} # TODO: bitMEX does not return any expired markets
-
-# Perpetuals
-ftx_symbols = ['BTC/USD:USD', 'ETH/USD:USD', 'SOL/USD:USD', 'CRO/USD:USD', 'GLMR/USD:USD', 'BNB/USD:USD', 'LTC/USD:USD']
-ftx_futures = ['BTC/USD:USD-220624']
-
-## To get historical data for expired contracts, use exchange specific symbols
+params_bitmex = {'market_name': 'ETHJ17'} # some exchanges do not return expired contracts, so you have to look them up yourself (e.g. bitmex)
 params_ftx = {'market_name': 'BTC-0325'}
 
 msec = 1000
@@ -41,40 +38,32 @@ def get_historical_data():
     now = exchange.milliseconds()
     while from_timestamp < now:
 
-        bars = exchange.fetch_ohlcv(ftx_symbols[0], '1m', from_timestamp)
-        # funding = exchange.fetch_funding_rate_history("BTC-PERP", from_timestamp, now) ##  Must pass in the exchange specific ID for this function
-        # print(exchange.last_response_headers)
+                                    #change value in first param to suit your exchange and symbol
+        bars = exchange.fetch_ohlcv(ftx_symbols[0], '1m', from_timestamp) # change timeframes as needed 
         time.sleep(exchange.rateLimit / msec)  
         print('Getting data from:', datetime.fromtimestamp(from_timestamp/1000))
 
         df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-        # df2 = pd.DataFrame(funding)
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        # df['funding_rate_time'] = pd.to_datetime(df2['datetime'])
-        # df['funding_rate'] = df2['fundingRate']
-        # df = df.drop_duplicates
-        # df['previous_close'] = df['close'].shift(1)
         df.to_csv('1mTimeFrame.csv', mode='a', index=False, header=False)
-        from_timestamp += len(bars) * minute
+        from_timestamp *= minute
         
     print('Finished getting historical data')
 
 def get_expired_historical_data():
     from_timestamp = exchange.parse8601('2022-03-22 00:00:00') 
-    end = exchange.parse8601('2022-03-25 00:00:00') # End date of the contract
-    while from_timestamp < end:
-
-        ## Params overrides the first paramter, so that can be left blank
+    end = exchange.parse8601('2022-06-22 00:00:00') # End date of the contract
+    while from_timestamp <= end:
+                                    # Params overrides the first paramater, so that can be left blank
+                                                                    # value value of last paramater
         bars = exchange.fetch_ohlcv('','1m', from_timestamp, end, params_ftx)
         time.sleep(exchange.rateLimit / msec)  
         print('Getting data from:', datetime.fromtimestamp(from_timestamp/1000))
 
         df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        # df = df.drop_duplicates
-        # df['previous_close'] = df['close'].shift(1)
         df.to_csv('expired.csv',  mode='a', index=False, header=False)
-        from_timestamp += len(bars) * minute
+        from_timestamp *= minute
 
     print('Finished getting historical data for expired contract')
 
@@ -95,12 +84,13 @@ def get_historical_trades():
         df.to_csv('trades.csv', mode='a', index=False, header=False)
         from_timestamp += 3600 * 1000 # increment by one hour
 
+# call functions here
+def main():
+    # get_historical_data()
+    get_expired_historical_data()
+    # get_historical_trades()
 
-get_historical_data()
-# get_expired_historical_data()
-# get_historical_trades()
-
-
+#TODO: timeframe resampler
 ## resample lower timeframe data into higher timeframes
 # data = df.resample('15T', on='timestamp').agg({'open': 'first',
 #                                               'high': 'max',
